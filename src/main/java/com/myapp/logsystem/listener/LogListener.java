3724dc2ac19dea68a78e46626a589e43f5533d1a
mudam5 <prashanthmudam4@gmail.com>
2025-09-03 17:41:07 +0530

    a

---
+++ src/main/java/com/myapp/logsystem/listener/LogListener.java
@@ -0,0 +1,51 @@
+package myapp.logsystem.listener;
+
+import java.util.concurrent.BlockingQueue;
+
+public class LogListener implements Runnable {
+
+    private final BlockingQueue<String> queue;
+
+    private String latestLog;
+
+    public LogListener(BlockingQueue<String> queue) {
+
+        this.queue = queue;
+
+    }
+
+    public String getLatestLog() {
+
+        return latestLog;
+
+    }
+
+    @Override
+
+    public void run() {
+
+        try {
+
+            while (true) {
+
+                String log = queue.take();
+
+                if ("STOP".equals(log)) break;
+
+                latestLog = log;
+
+                System.out.println("[Listener] Received: " + log);
+
+            }
+
+        } catch (InterruptedException e) {
+
+            Thread.currentThread().interrupt();
+
+        }
+
+    }
+
+}
+ 
+
