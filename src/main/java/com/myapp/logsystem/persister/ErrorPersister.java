659d5eb79b1b21df4c9ff15f6ef720d9962ef822
mudam5 <prashanthmudam4@gmail.com>
2025-09-03 18:05:47 +0530

    Update ErrorPersister.java

--- src/main/java/com/myapp/logsystem/persister/ErrorPersister.java
+++ src/main/java/com/myapp/logsystem/persister/ErrorPersister.java
@@ -1,6 +1,6 @@
 package myapp.logsystem.persister;
 public class ErrorPersister extends BasePersister {
-   public void persist(String log) {
-       writeToFile("logs/error.log", log);
+   public ErrorPersister() {
+       super("/var/log/logsystem/error.log");  // ✅ passes file path
    }
 }
