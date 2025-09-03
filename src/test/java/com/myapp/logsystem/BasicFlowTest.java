7999c0d2f2ca133c3cdd30900083afbee479dba2
mudam5 <prashanthmudam4@gmail.com>
2025-09-03 17:30:21 +0530

    Update BasicFlowTest.java

--- src/test/java/com/myapp/logsystem/BasicFlowTest.java
+++ src/test/java/com/myapp/logsystem/BasicFlowTest.java
@@ -1,1 +1,9 @@
-package com.mycompany.logsystem; public class BasicFlowTest {}
+package myapp.logsystem;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class BasicFlowTest {
+   @Test
+   public void testBasic() {
+       assertTrue(true, "Basic test runs");
+   }
+}
