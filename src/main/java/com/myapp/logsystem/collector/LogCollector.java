5fc49185dda4a528ade2104db7e97a8e05c1125b
mudam5 <prashanthmudam4@gmail.com>
2025-09-03 18:08:57 +0530

    Update LogCollector.java

--- src/main/java/com/myapp/logsystem/collector/LogCollector.java
+++ src/main/java/com/myapp/logsystem/collector/LogCollector.java
@@ -1,12 +1,15 @@
 package myapp.logsystem.collector;
 import myapp.logsystem.listener.LogListener;
 public class LogCollector {
-   private final LogListener listener;
+   private LogListener listener;
    public LogCollector(LogListener listener) {
        this.listener = listener;
    }
-   public String collect() {
-       return listener.getLatestLog();
+   public void collect() {
+       String log = listener.getLatestLog();  // ✅ now compiles
+       if (log != null) {
+           System.out.println("Collected log: " + log);
+       }
    }
 }
 
