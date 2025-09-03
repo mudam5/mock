b757af35782df8857b8cf50dc72a9f1dcb48bae0
mudam5 <prashanthmudam4@gmail.com>
2025-09-03 18:01:54 +0530

    Update BasePersister.java

--- src/main/java/com/myapp/logsystem/persister/BasePersister.java
+++ src/main/java/com/myapp/logsystem/persister/BasePersister.java
@@ -2,9 +2,13 @@ package myapp.logsystem.persister;
 import java.io.FileWriter;
 import java.io.IOException;
 public abstract class BasePersister {
-   protected void writeToFile(String filename, String log) {
-       try (FileWriter writer = new FileWriter(filename, true)) {
-           writer.write(log + "\n");
+   protected String filePath;
+   public BasePersister(String filePath) {
+       this.filePath = filePath;
+   }
+   public void persist(String message) {
+       try (FileWriter fw = new FileWriter(filePath, true)) {
+           fw.write(message + System.lineSeparator());
        } catch (IOException e) {
            e.printStackTrace();
        }
