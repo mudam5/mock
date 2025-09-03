4d00460249142017f626501d2740ad95c01b6265
mudam5 <prashanthmudam4@gmail.com>
2025-09-03 18:06:38 +0530

    Update DebugPersister.java

--- src/main/java/com/myapp/logsystem/persister/DebugPersister.java
+++ src/main/java/com/myapp/logsystem/persister/DebugPersister.java
@@ -1,6 +1,6 @@
 package myapp.logsystem.persister;
 public class DebugPersister extends BasePersister {
-   public void persist(String log) {
-       writeToFile("logs/debug.log", log);
+   public DebugPersister() {
+       super("/var/log/logsystem/debug.log");  // ✅ passes file path
    }
 }
